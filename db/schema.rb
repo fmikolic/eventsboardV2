478a27d613e4063f90456a44bd44ed3df340e997
fmikolic <mikolicfran@gmail.com>
2023-01-03 10:45:36 +0100

    Devise configuration, added users for org events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_02_113956) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_02_142910) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -32,6 +32,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_02_113956) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.string "organization_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
