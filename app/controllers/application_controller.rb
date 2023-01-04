78b3536f5d6c3832e946dacbcc7d73d6d9df5b8c
fmikolic <mikolicfran@gmail.com>
2023-01-04 09:46:03 +0100

    Included Pundit authorization gem, event_policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
+    include Pundit
+
+    after_action :verify_authorized, unless: :devise_controller?
     before_action :configure_permitted_parameters, if: :devise_controller?
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
 
     protected
 
@@ -11,5 +16,10 @@ class ApplicationController < ActionController::Base
         devise_parameter_sanitizer.permit(:account_update) do |user|
             user.permit(:email, :password, :password_confirmation, :current_password, :first_name, :last_name, :username, :organization_name)
         end
+    end
+
+    def user_not_authorized
+        flash[:alert] = "You are not authorized to perform that action!"
+        redirect_to(request.referrer || root_path)
     end
 end
