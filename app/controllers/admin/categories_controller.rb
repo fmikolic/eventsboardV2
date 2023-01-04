23441144e644e6bb1da2de1f77c814d15547e822
fmikolic <mikolicfran@gmail.com>
2023-01-04 15:51:07 +0100

    Added edit, delete for categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,8 @@
 class Admin::CategoriesController < Admin::ApplicationController
+    before_action :set_category, only: [:edit, :update, :destroy]
+    
     def index
-        
+        @categories = Category.all
     end
 
     def new
@@ -14,7 +16,7 @@ class Admin::CategoriesController < Admin::ApplicationController
             flash[:notice] = "Category has been created."
             redirect_to admin_categories_path
         else
-            flas[:alert] = "Category not created!"
+            flash[:alert] = "Category not created!"
             render "new"
         end
     end
@@ -24,16 +26,28 @@ class Admin::CategoriesController < Admin::ApplicationController
     end
 
     def update
-    
+        if @category.update(category_params)
+            flash[:notice] = "Category has been updated."
+            redirect_to admin_categories_path
+        else
+            flash[:alert] = "Category has not been update!"
+            render "edit"
+        end
     end
 
     def destroy
-    
+        @category.destroy
+        flash[:alert] = "Category has been deleted!"
+        redirect_to admin_categories_path
     end
 
     private
 
     def category_params
         params.require(:category).permit(:name, :summary)
+    end
+
+    def set_category
+        @category = Category.find(params[:id])
     end
 end
