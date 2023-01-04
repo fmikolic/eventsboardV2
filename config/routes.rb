64a6b9577e04fd7788920479fcb145a8d24332cf
fmikolic <mikolicfran@gmail.com>
2023-01-04 15:27:24 +0100

    Added form to create categories

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   namespace :admin do
     root "application#index"
     resources :users, only: [:index]
+    resources :categories, only: [:index, :new, :create, :edit, :update, :destroy]
   end
+
   devise_for :users
+
   resources :events
   resources :users, only: [:show]
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_04_090519) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_04_133823) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "summary"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.string "description"
