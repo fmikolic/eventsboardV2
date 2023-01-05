03763f483e44c8593765700977c1966d55080d9e
fmikolic <mikolicfran@gmail.com>
2023-01-05 11:24:17 +0100

    Add ability to upload images for events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_04_133823) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_05_100923) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "summary"
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_04_133823) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
