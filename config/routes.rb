fb14edcee0bf5090599a4a2bda57e530f2dc24fa
fmikolic <mikolicfran@gmail.com>
2023-01-05 10:33:30 +0100

    Added categories in navigation, category show page for all users

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   resources :events
   resources :users, only: [:show]
+  resources :categories, only: [:show]
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
