a915a0d39d3ebf021a9b1e7ecb77513aac185f6f
fmikolic <mikolicfran@gmail.com>
2023-01-09 14:52:00 +0100

    Added association between categories and events

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,8 @@
 class Category < ApplicationRecord
     extend FriendlyId
     friendly_id :name, use: :slugged
-    validates :name, presence: true, length: { minimum: 5 }
+    validates :name, presence: true, length: { minimum: 5 }, uniqueness: true
+    validates_uniqueness_of :name
+
+    has_many :events
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_05_111340) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_09_113424) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "summary"
@@ -32,6 +32,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_05_111340) do
     t.integer "user_id"
     t.string "image"
     t.string "slug"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_events_on_category_id"
     t.index ["slug"], name: "index_events_on_slug", unique: true
     t.index ["user_id"], name: "index_events_on_user_id"
   end
