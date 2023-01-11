b26819eea867855576b7f6c3167991647216061a
fmikolic <mikolicfran@gmail.com>
2023-01-11 11:05:54 +0100

    Included tags on events, searchable by tags

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
   resources :categories, only: [:show]
+  resources :tags, only: [:show]
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
